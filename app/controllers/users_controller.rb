fe1305b051546dd901499a0a46d9539a499388fe
Sheena Bonilla <sheena.myb@gmail.com>
2022-01-09 22:40:45 -0800

    Create new user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
     def new
+        @user = User.new
     end
 
     def create
+        @user = User.new(user_params)
+        binding.pry
     end
-    
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :email, :password_digest)
+    end
+
 end
