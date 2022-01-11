53040c5389a0e5bc3c770a6e9d42d22510af7906
Sheena Bonilla <sheena.myb@gmail.com>
2022-01-11 15:52:42 -0800

    Create views for organization

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -9,10 +9,4 @@ class ProgramsController < ApplicationController
             :name, :cover_area, :languages, :cost, :description, :eligibility
             )
     end
-    t.string "name"
-    t.text "coverage_area"
-    t.text "languages"
-    t.string "cost"
-    t.text "description"
-    t.text "eligibility"
 end
