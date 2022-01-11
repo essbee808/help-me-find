d55c49110b717cf2977c983eb1dbcd85580d761c
Sheena Bonilla <sheena.myb@gmail.com>
2022-01-11 15:49:28 -0800

    Create strong params in programs controller

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,4 +1,18 @@
 class ProgramsController < ApplicationController
     def new
     end
+
+    private
+
+    def program_params
+        params.require(:organization).permit(
+            :name, :cover_area, :languages, :cost, :description, :eligibility
+            )
+    end
+    t.string "name"
+    t.text "coverage_area"
+    t.text "languages"
+    t.string "cost"
+    t.text "description"
+    t.text "eligibility"
 end
