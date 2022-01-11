7f79cafc1a27bf2a5060535e7ad93e36de0dab08
Sheena Bonilla <sheena.myb@gmail.com>
2022-01-11 15:47:55 -0800

    Create strong params

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,4 +1,16 @@
 class OrganizationsController < ApplicationController
     def new
     end
+
+    def create
+    end
+
+    def index 
+    end
+
+    private
+
+    def organization_params
+        params.require(:organization).permit(:name, :website, :phone, :email)
+    end
 end
