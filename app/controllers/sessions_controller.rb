dfb3a85a4403f81b6bfea4c2483e0d85e773c19b
Sheena Bonilla <sheena.myb@gmail.com>
2022-01-11 00:04:21 -0800

    Add sessions helper methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,11 @@ class SessionsController < ApplicationController
 
   def create
     binding.pry
+    user = User.find_by(email: params[:session][:email])
+    
+    if user && user.authenticate(params[:session][:password])
+    else
+    end
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,21 +5,23 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
-        binding.pry
-        if @user && @user.authenticate(password: params[:password])
-            
+        #binding.pry
+        if @user.save && @user.authenticate(password: params[:user][:password])
+           session[:user_id] = @user.id 
+           redirect_to @user
         else
-
+           render :new
         end
     end
 
     def show
+        @user = User.find_by(id: params[:id])
     end
 
     private
 
     def user_params
-        params.require(:user).permit(:first_name, :last_name, :email, :password_digest)
+        params.require(:user).permit(:first_name, :last_name, :email, :password)
     end
 
 end
