83befc742a7af7dc74c8e6dae130462b40137351
Sheena Bonilla <sheena.myb@gmail.com>
2022-01-13 13:24:38 -0800

    Add validations to program model

--- app/models/program.rb
+++ app/models/program.rb
@@ -2,4 +2,6 @@ class Program < ApplicationRecord
     belongs_to :category
     belongs_to :organizer
     belongs_to :user
+
+    validates :name, :coverage_area, :langugages, :cost, :description, :eligibility, presence: true
 end
