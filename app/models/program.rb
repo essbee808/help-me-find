bc9c09b487a6f08337fb4bb830c398665f4c542f
Sheena Bonilla <sheena.myb@gmail.com>
2022-01-21 20:29:50 -0800

    Debug validation statement

--- app/models/program.rb
+++ app/models/program.rb
@@ -1,8 +1,8 @@
 class Program < ApplicationRecord
     belongs_to :category
-    belongs_to :organizer
+    belongs_to :organization
     belongs_to :user
 
-    validates :name, :coverage_area, :languages, :cost, :description, :eligibility, presence: true
+    validates :name, :coverage_area, :description, :website, presence: true
     validates :name, uniqueness: true
 end
