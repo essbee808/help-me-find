fe31e10de9f179ba6611325aa70a3dca3eb480a5
Sheena Bonilla <sheena.myb@gmail.com>
2022-01-23 18:16:04 -0800

    Add skip before action method to sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
 class ApplicationController < ActionController::Base
  include SessionsHelper
- 
+ before_action :require_login
+
  private
 
  def require_login
     unless logged_in?
         flash[:error] = "Please log in."
-        redirect_to login_url
+        redirect_to root_path
     end
  end
 end
